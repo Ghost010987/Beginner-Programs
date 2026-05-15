88743ce158fd2d593fe8eaa11740bac900181e63
Ghost010987 <gean.depp@gmail.com>
2026-05-15 19:59:46 +0530

    Optimize remove_duplicates function
    Refactored remove_duplicates function to use dict.fromkeys for improved efficiency.

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,12 +1,12 @@
 # Function to remove duplicates from list
 """Function with parameter original_list from which duplicates will be removed."""
+"""The below code runs with linear time complexity of O(n)"""
+	# dict.fromkeys() creates a dictionary with list items as keys.
+    # Since dict keys must be unique, duplicates are automatically removed.
+    # list() then converts those unique keys back into a list.
 def remove_duplicates(original_lst):
-					new_lst = []
-					for items in original_lst:
-									if items not in new_lst:
-													new_lst.append(items)
-					return new_lst
-
+	return list(dict.fromkeys(original_list))
+					
 def list_data():
 		user_input = list(map(int, input("Enter the data: ").strip(",")))
 		return user_input
